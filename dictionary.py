559853c0d39829e29bec9f09e911859fca740298
tmabizela023 <tmabizela023@student.wethinkcode.co.za>
2025-03-03 17:58:08 +0200

    included an exit option and convert user input to lower case before searching

--- dictionary.py
+++ dictionary.py
@@ -4,6 +4,7 @@ def load_data(file_path):
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
+            data = {key.lower(): value for key, value in data.items()}
         return data
     except FileNotFoundError:
         print("Error: The file was not found.")
@@ -11,6 +12,16 @@ def load_data(file_path):
     except json.JSONDecodeError:
         print("Error: The file is not a valid JSON.")
         return None
+
+def get_user_input():
+    while True:
+        word = input("Enter the word you want to search (or type 'exit' to quit): ").strip()
+        if word.lower() == 'exit':
+            return None
+        elif not word:
+            print("Error: No word entered. Please try again.")
+        else:
+            return word
 
 def translate(word, data):
     if data is None:
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import dictionary
 def main():
     data = dictionary.load_data("data.json")
 
-    word = input("Enter the word you want to search: ").strip()
-
-    if not word:
-        print("Error: No word entered. Please try again.")
-    else:
+    while True:
+        word = dictionary.get_user_input()
+        if word is None:
+            print("Exiting the program. Goodbye!")
+            break
         output = dictionary.translate(word, data)
 
         if isinstance(output, list):
@@ -16,7 +16,6 @@ def main():
                 print(item)
         else:
             print(output)
-
 
 if __name__ == "__main__":
     main()
