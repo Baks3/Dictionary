b1457a1c2555550596949a72b6b1a7e58c7de488
tmabizela023 <tmabizela023@student.wethinkcode.co.za>
2025-03-03 12:27:25 +0200

    added a simple dictionary file to check words

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+import json
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "Word cannot be found!"
+
+
+
+word = input("Enter the word you want to search")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
